a94e76e6a22dbeb59d2ff3c8b2a0d7b6d8700f6a
julius1932 <30441147+julius1932@users.noreply.github.com>
2018-06-11 18:20:09 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express=require('express');
-var path =require(path);
+var path =require('path');
 var exhbs=require('express-handlebars');
-
+var data = require('./aa');
+var dat2 = require('./hom');
 var app=express();
 
 app.set('views',path.join(__dirname,'views'));
@@ -11,7 +12,19 @@ app.set('view engine','handlebars');
 app.set('port',(process.env.PORT||3000));
 
 app.get('/',function(req,res){
-  res.render(home);
+  res.render('home',data);
+});
+
+app.get('/hom',function(req,res){
+/*  var meds =dat2.session.medication.meds;
+  var keys =Object.keys( meds);
+  console.log(keys.length);
+  var arr2=[];
+  for(var key in keys){
+    arr2.push(meds[key].medication);
+  }
+  dat2.medicns=arr2;*/
+  res.render('hom',dat2);
 });
 app.listen(app.get('port'),function(){
    console.log('Server Started on port '+app.get('port'));
